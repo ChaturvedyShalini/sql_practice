cc16e9e540af911e4d909c3d96a538ceff605fa3
SHALINI CHATURVEDY <154950054+ChaturvedyShalini@users.noreply.github.com>
2026-03-29 12:10:14 +0530

    Add SQL queries
    Add a query to categorize sales by shift and count orders.

--- retail_sales.sql
+++ retail_sales.sql
@@ -226,6 +226,28 @@ GROUP BY category;
 
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
 
+-- SELECT EXTRACT (HOUR FROM CURRENT_TIME)  -- TO KNOW THE CURRENT TIME
+
+WITH hourly_sale
+AS
+(
+SELECT *,
+	CASE
+		WHEN  EXTRACT (HOUR FROM sale_time) < 12 THEN 'Morning'
+		WHEN  EXTRACT (HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+	END as shift
+FROM retail_sales
+)
+SELECT
+	shift,
+	COUNT(*) as total_orders
+FROM hourly_sale
+GROUP BY shift
+
+
+
+
 
 
  
