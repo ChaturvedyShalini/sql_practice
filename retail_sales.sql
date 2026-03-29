585e177bc60e868c2a7f6e281c1417886812affb
SHALINI CHATURVEDY <154950054+ChaturvedyShalini@users.noreply.github.com>
2026-03-29 12:08:12 +0530

    Add SQL queries
    Added SQL queries to analyze transactions by gender and calculate average sales per month.

--- retail_sales.sql
+++ retail_sales.sql
@@ -170,7 +170,35 @@ WHERE total_sale > 1000;
 
 
 -- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+SELECT 
+	category,
+	gender,
+	COUNT(*) as total_trans
+FROM retail_sales
+GROUP 
+	BY 
+	category,
+	gender
+ORDER BY 1
+
+
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+SELECT 
+		year,
+		month,
+	avg_sale
+FROM
+(
+SELECT
+	EXTRACT(YEAR FROM sale_date) as year,
+	EXTRACT (MONTH FROM sale_date) as month,
+	AVG(total_sale) as avg_sale,
+	RANK() OVER(PARTITION BY EXTRACT(YEAR FROM sale_date) ORDER BY AVG(total_sale) DESC) as rank 
+FROM retail_sales
+GROUP BY 1, 2
+) as t1
+WHERE rank = 1
+
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
 -- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
